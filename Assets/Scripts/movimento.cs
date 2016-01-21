a73fc5a35b6c4ec02dce6b3ab354a1da7f676c15
raphalc <raphalc03@gmail.com>
2016-01-21 19:30:35 -0200

    Orientado a objetos
    Criação da classe Obstaculo Móvel

--- Assets/Scripts/movimento.cs
+++ Assets/Scripts/movimento.cs
@@ -1,31 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class movimento : MonoBehaviour {
+public class movimento : MonoBehaviour
+{
+    
+    public class ObstaculoMovel
+    {
+        private bool praCima;
+        public GameObject obst;
+
+        public ObstaculoMovel(bool pc)
+        {
+            this.praCima = pc;
+                  
+        }
+        public void movimentoVertical()
+        {
+            print(obst.transform.localPosition);
+            print("antes");
+            if (obst.transform.localPosition.y <= 3.0f && this.praCima)
+                obst.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+
+            if (obst.transform.localPosition.y >= 3.0f)
+            {
+                this.praCima = false;
+                print("topo");
+                print(obst.transform.localPosition.y);
+            }
+
+            if (!this.praCima)
+            {
+                print("Pra baixo");
+                if (obst.transform.localPosition.y <= 0.9f)
+                    this.praCima = true;
+                else
+                    obst.transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+                //obst.transform.localPosition =  Vector3.down * Time.deltaTime;
+            }
+        }
+    }
 
     // Use this for initialization
-    bool praCima;
-	void Start () {
-        praCima = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        print(transform.localPosition);
-        if (transform.localPosition.y <= 3.0f && praCima)
-            transform.Translate(Vector3.up * Time.deltaTime, Space.World);
-        else if (transform.localPosition.y > 3.0f)
-        {
-            praCima = false;
-            print("topo");
-            print(transform.localPosition.y);
-        }
-        else if (!praCima)
-        {
-            if (transform.localPosition.y <= 0.0f)
-                praCima = true;
-            else
-                transform.Translate(Vector3.down * Time.deltaTime, Space.World);
-        }
-   }
+
+    ObstaculoMovel oM = new ObstaculoMovel(true);
+
+    void Start()
+    {
+        GameObject obstaculo;
+        obstaculo = GameObject.Find("Obstaculo");
+        
+        oM.obst = obstaculo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oM.movimentoVertical();      
+        
+    }
 }
