670ca6f6271851fbc77c524ad3b593f834ddc64d
raphalc <raphalc03@gmail.com>
2016-01-22 20:29:01 -0200

    CalibraçãoPerfeita
    Comitando para a development line a correção do script movimento e sua
    calibragem

--- Assets/Scripts/movimento.cs
+++ Assets/Scripts/movimento.cs
@@ -1,31 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class movimento : MonoBehaviour {
+public class movimento : MonoBehaviour
+{
 
-    // Use this for initialization
-    bool praCima;
-	void Start () {
-        praCima = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        print(transform.localPosition);
-        if (transform.localPosition.y <= 3.0f && praCima)
-            transform.Translate(Vector3.up * Time.deltaTime, Space.World);
-        else if (transform.localPosition.y > 3.0f)
+    public class ObstaculoMovel
+    {
+        private bool praCima;
+        public GameObject obst;
+
+
+        public ObstaculoMovel(bool pc)
         {
-            praCima = false;
-            print("topo");
-            print(transform.localPosition.y);
+            this.praCima = pc;
         }
-        else if (!praCima)
+        public void movimentoVertical()
         {
-            if (transform.localPosition.y <= 0.0f)
-                praCima = true;
-            else
-                transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+            print(obst.transform.localPosition);
+            print("antes");
+            if (obst.transform.localPosition.y <= 3.0f && this.praCima)
+                obst.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+
+            if (obst.transform.localPosition.y >= 3.0f)
+            {
+                this.praCima = false;
+                print("topo");
+                print(obst.transform.localPosition.y);
+            }
+
+            if (!this.praCima)
+            {
+                print("Pra baixo");
+                if (obst.transform.localPosition.y <= 0.5f)
+                    this.praCima = true;
+                else
+                    obst.transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+                //obst.transform.localPosition =  Vector3.down * Time.deltaTime;
+            }
         }
-   }
+     }
+        // Use this for initialization
+
+        ObstaculoMovel oM = new ObstaculoMovel(true);
+
+        void Start()
+        {
+            GameObject obstaculo;
+            obstaculo = GameObject.Find("Obstaculo");
+
+            oM.obst = obstaculo;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            oM.movimentoVertical();
+
+        }
+  
 }
